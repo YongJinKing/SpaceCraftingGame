1c40f4215f1389725045636494732e18f239df6f
YongJinKing <dydwls120@gmail.com>
2024-05-16 12:01:45 +0900

    0516 제이손 구성 및 일부 이미지 삽입

---
+++ Assets/Script/System/Json/JsonData.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class NaturalResource_DataTable
+{
+
+}
+public class NaturalResource_StringTable
+{
+    
+}
+public class NaturalResource_IamgeResourceTable
+{
+    
+}
