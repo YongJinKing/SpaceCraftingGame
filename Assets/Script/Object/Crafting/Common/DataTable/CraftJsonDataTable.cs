77cac36a927eed2badcf59ccc7e65a8f44f3365a
LeeNamho <115772665+hs-1771178-leenamho@users.noreply.github.com>
2024-05-22 18:49:05 +0900

    0522 Json 사용을 위한 데이터테이블 스크립트 구상중

---
+++ Assets/Script/Object/Crafting/Common/DataTable/CraftJsonDataTable.cs
@@ -0,0 +1,28 @@
+struct CraftBuildingComponentTable
+{
+    public int ComponentDataTable_Index;
+    public int Component_Type;
+    public int Component_Name;
+    public int Component_Description;
+    public int Component_ImgIdx;
+    public int Component_Hp;
+    public int Component_Detail;
+}
+
+struct CraftBuildingAbilityTable
+{
+    public int ComponentDataTable_Index;
+    public int[] Consume_Index;
+    public int[] Consume_Count;
+    public int BuildingSpeed;
+    public int BuildingScale;
+    public int BuidlingDetale_Value;
+    public int BuidlingDetale_Delay;
+    public int BuildingDetail_Range;
+}
+
+struct CraftBuildImageTable
+{
+    public int ComponentDataTable_Index;
+    public string ImageResource_Name;
+}
