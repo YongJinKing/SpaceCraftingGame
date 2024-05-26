2e880fe80c9151fe4880997b61803ee3359f492f
YongJinKing <dydwls120@gmail.com>
2024-05-27 01:00:14 +0900

    0527단위 표시 초안

---
+++ Assets/Script/UI/Funtion/UnitCalculate.cs
@@ -0,0 +1,60 @@
+
+using UnityEngine;
+public class UnitCalculate
+{
+    private static UnitCalculate instance;
+    
+    private UnitCalculate()
+    {
+
+    }
+    public static UnitCalculate GetInstance()
+    {
+        if(UnitCalculate.instance == null)
+            UnitCalculate.instance = new UnitCalculate();
+        return UnitCalculate.instance;
+    }
+
+    public string Calculate(int Value)
+    {
+        string TranslateValue = "";
+        TranslateValue = Value.ToString();
+        if(Value >= 1000)
+        {
+            float TempValue;
+            TempValue = Mathf.Floor(((float)Value / 1000.0f) * 100.0f) / 100.0f;
+            TranslateValue = TempValue.ToString() + "k";
+            if((Mathf.Floor(((float)Value / 1000.0f) * 100.0f) / 100.0f) % 1 == 0)
+            {
+                TranslateValue = TempValue.ToString() + ".00k";
+            }
+            
+            if(Value >= 10000)
+            {
+                TempValue = Mathf.Floor(((float)Value / 1000.0f) * 10.0f) / 10.0f;
+                TranslateValue = TempValue.ToString() + "k";
+                if(Value >= 100000)
+                {
+                    TempValue = Mathf.Floor((float)Value / 1000.0f);
+                    TranslateValue = TempValue.ToString() + "k";
+                    if(Value >= 1000000)
+                    {
+                        TempValue = Mathf.Floor(((float)Value / 1000000.0f) * 100.0f) / 100.0f;
+                        TranslateValue = TempValue.ToString() + "m";
+                        if(Value >= 10000000)
+                        {
+                            TempValue = Mathf.Floor(((float)Value / 1000000.0f) * 10.0f) / 10.0f;
+                            TranslateValue = TempValue.ToString() + "m";
+                            if(Value >= 100000000)
+                            {
+                                TempValue = Mathf.Floor((float)Value / 1000000.0f);
+                                TranslateValue = TempValue.ToString() + "m";
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return TranslateValue;
+    }
+}
--- Assets/Script/UI/Inventory/InventoryPopup.cs
+++ Assets/Script/UI/Inventory/InventoryPopup.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -28,3 +29,4 @@ public class InventoryPopup : MonoBehaviour
     }
     
 }
+
