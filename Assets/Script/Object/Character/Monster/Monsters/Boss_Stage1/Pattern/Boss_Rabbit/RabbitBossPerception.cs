4acc3e18732c8bb3e7caa502d02b720177ce5afc
LeeNamho <115772665+hs-1771178-leenamho@users.noreply.github.com>
2024-06-18 15:48:06 +0900

    0618 절구통 레이어 생성 및 특수패턴 중간 저장

---
+++ Assets/Script/Object/Character/Monster/Monsters/Boss_Stage1/Pattern/Boss_Rabbit/RabbitBossPerception.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitBossPerception : MonoBehaviour
+{
+    public List<GameObject> mortalBoxes = new List<GameObject>();
+    public LayerMask mortalBoxLayerMask;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & mortalBoxLayerMask) != 0)
+        {
+            mortalBoxes.Add(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & mortalBoxLayerMask) != 0)
+        {
+            if (!mortalBoxes.Contains(collision.gameObject))
+            {
+                mortalBoxes.Add(collision.gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & mortalBoxLayerMask) != 0)
+        {
+            mortalBoxes.Remove(collision.gameObject);
+        }
+    }
+}
