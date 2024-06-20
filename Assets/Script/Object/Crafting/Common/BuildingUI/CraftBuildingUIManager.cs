50f22acb20336b71f4f5bf6827a614832faeec0c
Setang <128762532+setangK@users.noreply.github.com>
2024-06-20 17:03:28 +0900

    아몰라 짬뽕중

--- Assets/Script/Object/Crafting/Common/BuildingUI/CraftBuildingUIManager.cs
+++ Assets/Script/Object/Crafting/Common/BuildingUI/CraftBuildingUIManager.cs
@@ -1,33 +1,25 @@
-using Spine;
-using System.Collections;
+
 using System.Collections.Generic;
-using System.Drawing;
 using UnityEngine;
 using UnityEngine.Events;
-using static Unity.VisualScripting.Dependencies.Sqlite.SQLite3;
 using UnityEngine.UI;
-using System;
-using System.Reflection;
+
 
 
 public class CraftBuildingUIManager : MonoBehaviour
 {
-    public GameObject BuildingButton, InventoryButton;
-    public int ButtonClick, BuildButtonStatus, InventoryButtonStatus;
-
+    
+  
     public string curType = "Resource";
     //public List<Building> AllItemList, MyItemList,CurItemList;
-    public GameObject[] Slot, UsingImage;
-    public Image[] TabImage, ItemImage;
+   
+    public Image[] TabImage;
     public Sprite TabSelectSprite, TabDeselectSprite;
-    public Sprite[] ItemSprite;
-
-    [SerializeField] int BuildUIIndex;
-    CraftingUI BuildSet;
-
    
     public static CraftBuildingUIManager instance;
-    
+
+    public UnityEvent<int> UpdateBuildPopup;
+
     BuildingUISelectType BuildingType;
  
     public List<int> TypeID;
@@ -39,13 +31,10 @@ public class CraftBuildingUIManager : MonoBehaviour
         
 
         BuildingUIStructure.GetInstance().LoadBuildingInfo();
-        ButtonClick = 0;
-        BuildButtonStatus = 0;
-        InventoryButtonStatus = 0;
+        
         //BuildingButton.SetActive(false);
         //InventoryButton.SetActive(false);
-        BuildSet = new CraftingUI();
-        BuildUIIndex = 110000;
+     
         BuildingType = BuildingUISelectType.Resource;
 
 
@@ -68,82 +57,13 @@ public class CraftBuildingUIManager : MonoBehaviour
     {
 
 
-        if (Input.GetKey(KeyCode.B))
-        {
-            if (ButtonClick == 0)
-            {
-                Debug.Log("건설 창 오픈");
-                BuildUIOpen();
-            }
-            if (ButtonClick == 1)
-            {
-                Debug.Log("건설 창 클로즈");
-                BuildUIClose();
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.I))
-        {
-            if (ButtonClick == 0)
-            {
-                Debug.Log("건설 창 오픈");
-                InventoryUIOpen();
-            }
-            if (ButtonClick == 1)
-            {
-                Debug.Log("건설 창 클로즈");
-                InventoryUIClose();
-            }
-        }
-
-
     }
-    /*
-    public void SlotClick(int slotNum)
-    {
-        Building CurItem = CurItemList[slotNum];
-        Building UsingItem = CurItemList.Find(x => x.isUsing == true);
 
-        if (curType == "Resource")
-        {
-            //무조건 하나만 선택
-            if (UsingItem != null) UsingItem.isUsing = false;
-            CurItem.isUsing = true;
-        }
-        else
-        {
-            //무조건 하나만 선택
-            if (UsingItem != null) UsingItem.isUsing = false;
-            CurItem.isUsing = true;
-            
-            //선택 해제가능
-            CurItem.isUsing = !CurItem.isUsing;
-            if (UsingItem != null) UsingItem.isUsing = false;
-            
-        }
-        
-        //   print(CurItemList[slotNum].Name);
-    }*/
 
     // 탭 버튼 변경
     public void TabClick(string tabName)
     {
         curType = tabName;
-        //CurItemList = MyItemList.FindAll(x => x.Type == tabName);
-        /*
-        for (int i = 0; i < Slot.Length; i++)
-        {
-            bool isExist = i < CurItemList.Count;
-            Slot[i].SetActive(i < CurItemList.Count);
-            //텍스트 이름 가져오기
-            Slot[i].GetComponentInChildren<Text>().text = i < CurItemList.Count ? CurItemList[i].Name : "";
-
-            if (isExist)
-            {
-                ItemImage[i].sprite = ItemSprite[AllItemList.FindIndex(x => x.Name == CurItemList[i].Name)];
-                UsingImage[i].SetActive(CurItemList[i].isUsing);
-            }
-        }*/
-
 
         int tabNum = 0;
         //탭 이미지 변경
@@ -159,59 +79,13 @@ public class CraftBuildingUIManager : MonoBehaviour
 
 
     }
-    /*
-    public void ResourceButtonClick()
-    {
-
-
-        BuildUIIndex = 100000;
-        Debug.Log("현재 건설 선택된 인덱스 : " + BuildUIIndex);
-        BringBuildData(BuildUIIndex);
-
-    }
-
-    public void TurretButtonClick()
-    {
-
-        BuildUIIndex = 110000;
-        Debug.Log("현재 건설 선택된 인덱스 : " + BuildUIIndex);
-        BringBuildData(BuildUIIndex);
-
-    }
-
-    public void BringBuildData(int index)
-    {
-        GameObject ResourceBuilding = BuildSet.CraftBuildUI(index);
-        //Debug.Log(ResourceBuilding);
-    }
-    */
-    public void BuildUIOpen()
-    {
-        BuildingButton.SetActive(true);
-        ButtonClick = 1;
-    }
-    public void BuildUIClose()
-    {
-        BuildingButton.SetActive(false);
-        ButtonClick = 0;
-    }
-
-    public void InventoryUIOpen()
-    {
-        InventoryButton.SetActive(true);
-        ButtonClick = 1;
-    }
-    public void InventoryUIClose()
-    {
-        InventoryButton.SetActive(false);
-        ButtonClick = 0;
-    }
-
+ 
+    
     public void ChangeMode(int index)
     {
         BuildingType = (BuildingUISelectType)index;
         BuildingTabFunction(BuildingType);
-        
+        UpdateBuildPopup?.Invoke(1);
     }
 
     public void BuildingTabFunction(BuildingUISelectType Type)
--- Assets/Script/UI/BuildingUI/BuildingUIPop.cs
+++ Assets/Script/UI/BuildingUI/BuildingUIPop.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class BuildingUIPop : MonoBehaviour
 {
     public GameObject objSlotGridLine;
+    public GameObject objSlotPopup;
+    Coroutine SlotPopup;
 
 
     // Start is called before the first frame update
@@ -21,6 +23,43 @@ public class BuildingUIPop : MonoBehaviour
 
     public void PopupBuildUpadate()
     {
-       // if(CraftBuildingUIManager.instance.ApplyTypeID.Count > )
+        for (int i = 0; i < objSlotGridLine.transform.childCount; i++)
+        {
+            if (CraftBuildingUIManager.instance.ApplyTypeID.Count > objSlotGridLine.transform.GetChild(i).GetSiblingIndex())
+            {
+                objSlotGridLine.transform.GetChild(i).GetComponent<BuildingUISlot>().InputSlotData();
+                objSlotGridLine.transform.GetChild(i).GetChild(0).gameObject.SetActive(true);
+            }
+            else
+            {
+                objSlotGridLine.transform.GetChild(i).GetChild(0).gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void SlotHL(int index, bool onCheck)
+    {
+        //getchild() 안에 숫자는 자식의 위치
+        objSlotGridLine.transform.GetChild(index).GetChild(2).gameObject.SetActive(onCheck);
+        if (onCheck)
+        {
+            SlotPopup = StartCoroutine(CorBuildSlotPopup(index));
+        }
+        else
+        {
+            if(SlotPopup != null)
+            {
+                StopCoroutine(SlotPopup);
+                SlotPopup = null;
+                objSlotPopup.SetActive(false);
+            }
+        }
+    }
+    IEnumerator CorBuildSlotPopup(int index)
+    {
+        yield return new WaitForSeconds(1);
+        objSlotPopup.SetActive(true);
+        objSlotPopup.transform.GetComponent<RectTransform>().anchoredPosition =
+            objSlotGridLine.transform.GetChild(index).GetComponent<RectTransform>().anchoredPosition;
     }
 }
