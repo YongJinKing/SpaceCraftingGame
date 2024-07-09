8ddceee76677da82306ce534225df163d1e70ad7
TaegeunJeung <wjdxorms840@gmail.com>
2024-07-09 16:08:00 +0900

    0709 데이터슬롯

--- Assets/Script/System/Json/DataManager.cs
+++ Assets/Script/System/Json/DataManager.cs
@@ -3,48 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
 using System.IO;
+using Spine;
+
+
+public class DataSlots : MonoBehaviour
+{
+    ResourcesSpawner resourcesSpawner = new ResourcesSpawner
+    {
+
+    };
+    
+    public void Start()
+    {
+        // json = ResourcesSpawner(resourcesSpawner);
+    }
+}
 
 public static class DataManager
 {
-    public static string serializeComponents(ComponetsInfo componetsInfo)
-    {
-        return JsonUtility.ToJson(componetsInfo);
-    }
-
-    public static string StatData(Stat stat)
-    {
-        return JsonUtility.ToJson(stat);
-    } 
-
-    public static string PlayerData(Player player)
-    {
-        return JsonUtility.ToJson(player);
-    }
-    
-    public static string WeaponData(Weapon weapon)
-    {
-        return JsonUtility.ToJson(weapon);
-    }
-
-    public static string MonsterData(Monster monster)
-    {
-        return JsonUtility.ToJson(monster);
-    }
-
-    public static string UnitData(Unit unit)
-    {
-        return JsonUtility.ToJson(unit);
-    }
-
-    public static string MonsterStateData(MonsterState monsterstate)
-    {
-        return JsonUtility.ToJson(monsterstate);
-    }
-
-    public static string TurretData(Turret turret)
-    {
-        return JsonUtility.ToJson(turret);
-    }
 
     public static string ResourcesSpawnerData(ResourcesSpawner resourcesSpawner)
     {
