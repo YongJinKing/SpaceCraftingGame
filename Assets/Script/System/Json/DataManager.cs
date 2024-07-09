bf4ad29204119eb9e41cf6cca70abe0db1f51a46
TaegeunJeung <wjdxorms840@gmail.com>
2024-07-09 17:58:44 +0900

    0709 플레이어 데이터 직렬화 중

--- Assets/Script/System/Json/DataManager.cs
+++ Assets/Script/System/Json/DataManager.cs
@@ -4,28 +4,77 @@ using UnityEngine;
 using Newtonsoft.Json;
 using System.IO;
 using Spine;
+using System;
 
-
-public class DataSlots : MonoBehaviour
+[Serializable]
+public class PlayerDataManager:MonoBehaviour
 {
-    ResourcesSpawner resourcesSpawner = new ResourcesSpawner
-    {
+    public int index;
+    public int HP;
+    public float moveSpeed;
+    public float ATK;
+    public float ATKSpeed;
 
-    };
-    
-    public void Start()
+}
+
+[Serializable]
+public class PlayerDataMansgerInfo
+{
+    public List<PlayerDataManager> playerDataManagers;
+
+    public PlayerDataMansgerInfo(List<PlayerDataManager> playerDataManagers)
     {
-        // json = ResourcesSpawner(resourcesSpawner);
+        this.playerDataManagers = playerDataManagers;
     }
 }
 
-public static class DataManager
+public class PlayerDataManagerSaveSystem : Singleton<PlayerDataManagerSaveSystem>
 {
-
-    public static string ResourcesSpawnerData(ResourcesSpawner resourcesSpawner)
+    PlayerDataMansgerInfo playerDataMansgerInfo;
+    string savePath;
+    public void SavePlayerInfo()
     {
-        return JsonUtility.ToJson(resourcesSpawner);
+        Unit unit = FindAnyObjectByType<Unit>();
+        List<PlayerDataManager> playerDataManagersList = new List<PlayerDataManager>();
+
+        playerDataMansgerInfo = new PlayerDataMansgerInfo(playerDataManagersList);
+        var json = JsonConvert.SerializeObject(playerDataMansgerInfo);
+        savePath = "SaveTestJson.json";
+        File.WriteAllText(savePath, json);
+    }
+
+    string LoadJson()
+    {
+        var jsonPath = "SaveTestJson.json";
+        if (File.Exists(jsonPath))
+        {
+            string JsonString = File.ReadAllText(jsonPath);
+            Debug.Log("읽기");
+            return JsonString;
+        }
+        else
+        {
+            Debug.Log("없음");
+            return string.Empty;
+        }
+    }
+
+    public PlayerDataMansgerInfo LoadPlayerInfo()
+    {
+        var jsonString = LoadJson();
+        if (string.IsNullOrEmpty(jsonString))
+        {
+            Debug.LogError("JSON 데이터가 비어 있습니다.");
+            return null;
+        }
+
+        PlayerDataMansgerInfo playerDataManagersList = JsonUtility.FromJson<PlayerDataMansgerInfo>(jsonString);
+        if(playerDataManagersList == null || playerDataManagersList.playerDataManagers == null)
+        {
+            Debug.LogError("JSON 데이터를 역직렬화하는데 실패했습니다.");
+        }
+
+        return playerDataManagersList;
+
     }
 }
-
-
