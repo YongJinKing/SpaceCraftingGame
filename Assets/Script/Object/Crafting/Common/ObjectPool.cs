836fbca95802522bd8c7070ab7d710aa3de69aa2
Razack <57121998+wsxed1230@users.noreply.github.com>
2024-08-20 15:44:29 +0900

    0820 오브젝트 풀 수정

--- Assets/Script/Object/Crafting/Common/ObjectPool.cs
+++ Assets/Script/Object/Crafting/Common/ObjectPool.cs
@@ -28,11 +28,40 @@ public class ObjectPool : Singleton<ObjectPool>
         return Instantiate(org, p);
     }
 
+    public GameObject GetObject(string Key, GameObject org, Transform p)
+    {
+        if (myPool.ContainsKey(Key))
+        {
+            if (myPool[Key].Count > 0)
+            {
+                GameObject obj = myPool[Key].Pop();
+                obj.SetActive(true);
+                obj.transform.SetParent(p);
+                obj.transform.localPosition = Vector3.zero;
+                obj.transform.rotation = p.rotation;
+                return obj;
+            }
+        }
+        return Instantiate(org, p);
+    }
+
     public void ReleaseObject<T>(GameObject obj)
     {
         obj.transform.SetParent(transform);
         obj.SetActive(false);
         string Key = typeof(T).ToString();
+
+        if (!myPool.ContainsKey(Key))
+        {
+            myPool[Key] = new Stack<GameObject>();
+        }
+
+        myPool[Key].Push(obj);
+    }
+    public void ReleaseObject(string Key, GameObject obj)
+    {
+        obj.transform.SetParent(transform);
+        obj.SetActive(false);
 
         if (!myPool.ContainsKey(Key))
         {
