20dea45b58c0ef0b49f91bb6b48fa251cff009c0
TaegeunJeung <wjdxorms840@gmail.com>
2024-08-21 20:11:54 +0900

    0821 중간저장2

--- Assets/Scenes/TaeGeun/DataView/GunManager.cs
+++ Assets/Scenes/TaeGeun/DataView/GunManager.cs
@@ -1,18 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class GunManager : MonoBehaviour
+public class GunManager : Singleton<GunManager>
 {
-    // Start is called before the first frame update
+    public int[] gunIndexes = new int[3];
+    public string savePath;
     void Start()
     {
-        
+        LoadGunIndexes();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SaveGunIndexs()
     {
-        
+        string json = JsonUtility.ToJson(new GunIndexData(gunIndexes));
+        File.WriteAllText(savePath, json);
+    }
+
+    public void UpdateGunIndex(int slot, int newIndex)
+    {
+        if(slot < 0 || slot>= gunIndexes.Length)
+        {
+            Debug.LogError("Invalid slot index");
+            return;
+        }
+        gunIndexes[slot] = newIndex;
+    }
+    public int GetGunIndex(int slot)
+    {
+        if(slot < 0 || slot >= gunIndexes.Length)
+        {
+            Debug.LogError("Invalid slot index");
+            return -1; // 유효하지 않은 경우 -1 반환
+        }
+
+        return gunIndexes[slot];
+    }
+
+    public void LoadGunIndexes()
+    {
+        if(File.Exists(savePath))
+        {
+            string json = File.ReadAllText(savePath);
+            GunIndexData data = JsonUtility.FromJson<GunIndexData>(json);
+            gunIndexes = data.indexes;
+        }
+        else
+        {
+            // 총기번호 인덱스 넣기
+            gunIndexes[0] = 10100;
+            gunIndexes[1] = 10200;
+            gunIndexes[2] = 10300;
+            SaveGunIndexs();
+        }
+    }
+
+    [Serializable]
+    private class GunIndexData
+    {
+        public int[] indexes;
+
+        public GunIndexData(int[] indexes)
+        {
+            this.indexes = indexes;
+        }
     }
 }
