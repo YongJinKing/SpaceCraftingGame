da77155c787c77791114f50e598954b09800e341
Setang <128762532+setangK@users.noreply.github.com>
2024-09-07 21:23:41 +0900

    Slot icons

--- Assets/Scenes/SeChan/MinimapTest/BaseCamp.cs
+++ Assets/Scenes/SeChan/MinimapTest/BaseCamp.cs
@@ -8,7 +8,7 @@ public class BaseCamp : MonoBehaviour
     // Start is called before the first frame update
 
     public GameObject OrgPos;
-
+    public Camera minimapCamera;
     void Start()
     {
         this.transform.position = OrgPos.transform.position;
@@ -18,13 +18,13 @@ public class BaseCamp : MonoBehaviour
     void Update()
     {
         this.transform.position = OrgPos.transform.position;
-        Vector3 viewportPosition = Camera.main.WorldToViewportPoint(transform.position);
+        Vector3 viewportPosition = minimapCamera.WorldToViewportPoint(transform.position);
         if (viewportPosition.x < 0 || viewportPosition.x > 1 || viewportPosition.y < 0 || viewportPosition.y > 1)
         {
             // 화면을 벗어날 경우, 화면 가장자리에 위치시키기
             this.GetComponent<SpriteRenderer>().enabled = true;
-            viewportPosition.x = Mathf.Clamp(viewportPosition.x, 0, 0.97f);
-            viewportPosition.y = Mathf.Clamp(viewportPosition.y, 0, 0.97f);
+            viewportPosition.x = Mathf.Clamp(viewportPosition.x, 0.03f, 0.97f);
+            viewportPosition.y = Mathf.Clamp(viewportPosition.y, 0.03f, 0.97f);
            
         }
         else
@@ -32,7 +32,7 @@ public class BaseCamp : MonoBehaviour
             this.GetComponent<SpriteRenderer>().enabled = false;
         }
     
-        Vector3 worldPosition = Camera.main.ViewportToWorldPoint(viewportPosition);
+        Vector3 worldPosition = minimapCamera.ViewportToWorldPoint(viewportPosition);
         this.transform.position = worldPosition;
 
 
