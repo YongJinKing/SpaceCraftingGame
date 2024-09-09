58e9b3347510f67cd063b56ca6c4a34bf73cc495
Setang <128762532+setangK@users.noreply.github.com>
2024-09-09 20:04:10 +0900

    Minimap수정

---
+++ Assets/Scenes/SeChan/MinimapTest/RaiderRotation.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaiderRotation : MonoBehaviour
+{
+
+    float rotSpeed = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, -rotSpeed * Time.deltaTime));
+    }
+}
