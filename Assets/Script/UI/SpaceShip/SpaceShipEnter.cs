546a98c6c50c4d2173050b3e34135fbcdc5a687d
LeeNamho <115772665+hs-1771178-leenamho@users.noreply.github.com>
2024-09-13 21:25:24 +0900

    0913 우주선 아웃라인 추가

--- Assets/Script/UI/SpaceShip/SpaceShipEnter.cs
+++ Assets/Script/UI/SpaceShip/SpaceShipEnter.cs
@@ -9,10 +9,31 @@ public class SpaceShipEnter : MonoBehaviour
     public GameObject invenCanvas;
     public GameObject buildingCanvas;
     public GameObject pauseCanvas;
+
+    public Material origin;
+    public Material mouseEnter;
+
+    public MeshRenderer meshRender;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity, spaceshipLayer);
+
+        if (hit.collider != null)
+        {
+            if (((1 << hit.collider.gameObject.layer) & spaceshipLayer) != 0)
+            {
+                //spaceShipCanvas.SetActive(true);
+                meshRender.material = mouseEnter;
+            }
+            else
+            {
+                meshRender.material = origin;
+            }
+        }
+
+        /*if (Input.GetMouseButtonDown(0))
         {
             if (invenCanvas.activeSelf || buildingCanvas.activeSelf || pauseCanvas.activeSelf) return;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -25,6 +46,6 @@ public class SpaceShipEnter : MonoBehaviour
                     spaceShipCanvas.SetActive(true);
                 }
             }
-        }
+        }*/
     }
 }
