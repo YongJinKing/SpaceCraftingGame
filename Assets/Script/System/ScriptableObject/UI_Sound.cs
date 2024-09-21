46e71b90712022dfb70c85634a9666e69a8eb735
Setang <128762532+setangK@users.noreply.github.com>
2024-09-21 17:04:39 +0900

    Sound추가

--- Assets/Script/System/ScriptableObject/UI_Sound.cs
+++ Assets/Script/System/ScriptableObject/UI_Sound.cs
@@ -6,6 +6,25 @@ using UnityEngine;
 
 public class UI_Sound : ScriptableObject
 {
-    public AudioClip Sound1;
-    //필요한 소리 바인딩
+    //재료 부족
+    public AudioClip NeedMoreResourceSFX;
+    //우주선 입장
+    public AudioClip EnterSpaceShipSFX;
+    //망치
+    public AudioClip HammeringSFX;
+    //곡괭이
+    public AudioClip MiningSFX;
+    //공구 변경
+    public AudioClip ChangeToolSFX;
+    //무기 변경
+    public AudioClip ChangeWeaponSFX;
+    //제작무기탭 변경
+    public AudioClip ChangingTabSFX;
+    //건설 탭 열기
+    public AudioClip OpenBuildingSFX;
+    //건물 제작 
+    public AudioClip BuildingSFX;
+    //강화 성공
+    public AudioClip SuccesSFX;
+    
 }
