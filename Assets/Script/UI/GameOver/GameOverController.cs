0f1cbeeefb2517c20fcdb5c3990cfafa6518def9
LeeNamho <115772665+hs-1771178-leenamho@users.noreply.github.com>
2024-09-22 16:30:50 +0900

    0922 게임오버 캔버스 추가

---
+++ Assets/Script/UI/GameOver/GameOverController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverController : MonoBehaviour
+{
+    [SerializeField] Button goBackToStartScene;
+
+    void OnEnable()
+    {
+        Time.timeScale = 0f;
+        goBackToStartScene.onClick.AddListener(GoBackToStartScene);
+    }
+
+    void GoBackToStartScene()
+    {
+        SceneManager.LoadSceneAsync(0);
+        Time.timeScale = 1f;
+    }
+}
